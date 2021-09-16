a24acd8d9ec2acf3e60a75510baf7af74548c4d2
Melgaidi <elgaidi@stibits.io>
2021-09-16 11:17:41 +0100

    P2PKH asset descriptor supported with Blockbook

--- bchain/coins/ravencoin/ravencoinparser.go
+++ bchain/coins/ravencoin/ravencoinparser.go
@@ -1,8 +1,12 @@
 package ravencoin
 
 import (
+	"encoding/binary"
+        "encoding/hex"
+
 	"github.com/martinboehm/btcd/wire"
 	"github.com/martinboehm/btcutil/chaincfg"
+	"github.com/martinboehm/btcutil/txscript"
 	"github.com/trezor/blockbook/bchain"
 	"github.com/trezor/blockbook/bchain/coins/btc"
 )
@@ -62,6 +66,41 @@ func GetChainParams(chain string) *chaincfg.Params {
 	default:
 		return &MainNetParams
 	}
+}
+
+// GetAddrDescFromVout returns internal address representation (descriptor) of given transaction output
+func (p *RavencoinParser) GetAddrDescFromVout(output *bchain.Vout) (bchain.AddressDescriptor, error) {
+        ad, err := hex.DecodeString(output.ScriptPubKey.Hex)
+        if err != nil {
+                return ad, err
+        }
+
+        l := len(ad)
+        if l > 25 {
+                if ad[0] == 0x76 &&  ad[1] == 0xa9 && ad[2] == 0x14 && ad[l-1] == 0x75{
+                        add := ad[0 : 25]
+                        return add, err
+                }
+        }
+
+        // convert possible P2PK script to P2PKH
+        // so that all transactions by given public key are indexed together
+        return txscript.ConvertP2PKtoP2PKH(p.Params.Base58CksumHasher, ad)
+}
+
+// GetAddressesFromAddrDesc returns addresses for given address descriptor (asset supported)  with flag if the addresses are searchable
+func (p *RavencoinParser) GetAddressesFromAddrDesc(addrDesc bchain.AddressDescriptor) ([]string, bool, error) {
+        var addressDesc bchain.AddressDescriptor
+        addressDesc = addrDesc
+        l := binary.Size(addrDesc)
+
+        if l > 25 {
+                if addrDesc[0] == 0x76 &&  addrDesc[1] == 0xa9 && addrDesc[2] == 0x14 && addrDesc[l-1] == 0x75 {
+                	addressDesc = addrDesc[0:25]
+                }
+        }
+
+        return p.BitcoinParser.GetAddressesFromAddrDesc(addressDesc)
 }
 
 // PackTx packs transaction to byte array using protobuf
